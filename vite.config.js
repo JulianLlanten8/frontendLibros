6755db0dcdb59e38810cb0b9b57db85aac02c2ee
Julian Llanten <hugojulians@gmail.com>
2023-06-15 22:20:46 -0500

    Add vite base

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig(({ command }) => {
     return {
         plugins: [vue()],
-        base: command === 'serve' ? '' : '/dist/',
+        base: command === 'serve' ? '' : 'deploy',
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url))
